da1fd24c6f6dcef2471ea5f7c919775e8f703862
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2025-07-25 17:13:06 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -27,8 +27,9 @@ firebase.auth.onAuthStateChanged((user) => {
     TrueTest.setSessionAttributes({
       email: user.email,
       providerId: user.providerData[0].providerId,
-      displayName: user.providerData[0].displayName
-      thisIsForTrueTestTesting123_long_key_display_name_heioHHDNM-test = '-test-add-tri@%#^$&!-!this_value_is_random_from_xu_029392_%@%@&_hello_uuid_value_test_1283839303838393'
+      displayName: user.providerData[0].displayName,
+      photoUrl: user.providerData[0].photoUrl,
+      thisIsForTrueTestTesting123_long_key_display_name_heioHHDNMTesting98 = '-test-add-tri@%#^$&!-!this_value_is_random_from_xu_029392_%@%@&_hello_uuid_value_test_1283839303838393'
     });
     store.dispatch(onAuthStateSuccess(user));
   } else {
