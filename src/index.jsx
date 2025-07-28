f4ba40ee05fded3578d2f4df186313b7794b112f
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2025-07-28 13:38:46 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -29,24 +29,24 @@ firebase.auth.onAuthStateChanged((user) => {
       providerId: user.providerData[0].providerId,
       displayName: user.providerData[0].displayName,
       photoUrl: user.providerData[0].photoUrl,
-      key-01_1#@%^$&*(#): 'abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !"§ $%& /() =?* '<> #|; ²³~ @`´ ©«» ¤¼× {} abc def ghi jkl mno pqrs tuv wxyz ABC D DEF',
-      key_02: 'abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !"§ $%& /(!"§ $%& /() =?* '<> #|; ²³~ @`´ ©«» ¤¼× {} abc def ghi jkl mno pqrs tuv wx',
-      key_03_testing_long_255_characters_key______________dummy_-----+__00384793739303!@%--d=djkdfgjksngkfshidufhgosdgnjkdfngjshfgusy4y5g5328492502hdisisisi_____dummy_-----+__003847937_____dummy_-----+__003847937_____dummy_-----+__003847937_____dummy_-----+__: 'key_02_testing_long_255_characters_key______________dummy_-----+__00384793739303!@%--d=djkdfgjksngkfshidufhgosdgnjkdfngjshfgusy4y5g5328492502hdisisisi_____dummy_-----+__003847937_____dummy_-----+__003847937_____dummy_-----+__003847937_____dummy_-----+__',
-      'key_04': 'testtinh_key 03 with space',
-      'key_05': '@#($8484994 10c9e703-16de-40af-8225-9af018adfa68',
-      'key_06_10c9e703-16de-40af-8225-9af018adfa68': 'session_timestampt_a651ad3e-91a9-4d25-aeab-54b638d55955',
-      'a651ad3e-91a9-4d25-aeab-54b638d55955': '0XE77D101C647747C488AA751376B08A06',
-      '1234456779594030393838383848949404847474948484': 1234456779594030393838383848949404847474948484,
-      'KEY_06': '0XE77D101C647747C488AA751376B08A06 0XA529143872B1446A9D98026B6EF22D1D'
-      thisIsForTrueTestTesting123_long_key_display_name_heioHHDNMTesting98: '~!@#$%^?-test-add-tri-!this_value_is_random_from_xu_029392_%@%@&_hello_uuid_value_test_1283839303838393'
+      key-01_1: 'abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !qrs tuv wxyz ABC D DEF',
+      key_02: 'abc def ghi jkl mno pqrs tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !§ $%& /() =?* '<> #|~ @`´{} abc def ghi jkl mno pqrs tuv wx',
+      key_03_testing_long_255_characters_key__djkdfgjksngkfshidufhgosdgnjkdfngjshfgusy4y----+__003847937_____dummy_-: 'key_02_testing_long_255_characters_key______________dummy_-----+__00384793739303!@%--d=28492502hdisisisi_____dummy_-----+__003847937_____dummy_-----+__003847937_____dummy_-----+__003847937_____dummy_-----+__',
+      key_04: 'testtinh_key 03 with space',
+      key_05: '@#($8484994 10c9e703-16de-40af-8225-9af018adfa68',
+      key_06_10c9e703-16de-40af-8225-9af018adfa68: 'session_timestampt_a651ad3e-91a9-4d25-aeab-54b638d55955',
+      a651ad3e-91a9-4d25-aeab-54b638d55955: '0XE77D101C647747C488AA751376B08A06',
+      1234456779594030393838383848949404847474948484: 1234456779594030393838383848949404847474948484,
+      KEY_06: '0XE77D101C647747C488AA751376B08A06 0XA529143872B1446A9D98026B6EF22D1D'
+      thisIsForTrueTestTesting123_long_key_display_name_heioHHDNMTesting98hello: '~!@#$%^?-test-add-tri-!this_value_is_random_from_xu_029392_%@%@&_hello_uuid_value_test_1283839303838393'
     });
     store.dispatch(onAuthStateSuccess(user));
   } else {
     TrueTest.setSessionAttributes({
       email: 'anonymous',
       providerId: 'no',
-      'key_05_10c9e703-16de-40af-8225-9af018adfa68' = 'session_timestampt_a651ad3e-91a9-4d25-aeab-54b638d55955',
-      'a651ad3e-91a9-4d25-aeab-54b638d55955' = '0XE77D101C647747C488AA751376B08A06'
+      key_05_10c9e703-16de-40af-8225-9af018adfa68: 'session_timestampt_a651ad3e-91a9-4d25-aeab-54b638d55955',
+      a651ad3e-91a9-4d25-aeab-54b638d55955: '0XE77D101C647747C488AA751376B08A06'
     });
     store.dispatch(onAuthStateFail('Failed to authenticate'));
   }
